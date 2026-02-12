d588762b30e5e80e00f16ae89a9c7447f82f52a0
fer <lfrecalde1@espe.edu.ec>
2026-02-12 11:50:00 -0500

    updates

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+if false then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "cpp",
+      "c",
+      "cmake",
+      "bash",
+      "lua",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
